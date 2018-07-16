623a51858577fc6f1b5ad1ef94fd51b3134914b1
Shamima Yeasmin <muktacseku@gmail.com>
2018-07-16 11:12:30 -0600

    First commit in the morningJuly 16

--- src/lucene/LuceneIndexer.java
+++ src/lucene/LuceneIndexer.java
@@ -3,6 +3,8 @@ package lucene;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.stream.Collectors;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -11,9 +13,16 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.util.Version; 
@@ -23,7 +32,7 @@ import org.apache.lucene.util.packed.PackedInts.Reader;
 
 public class LuceneIndexer {
 
-	String indexDir;
+	static String indexDir;
 	String docsDir;
 
 	public LuceneIndexer(String docFolder, String indexFolder) {
@@ -46,18 +55,17 @@ public class LuceneIndexer {
 		File docs = new File(this.docsDir);
 		File[] files = docs.listFiles();
 		for (File file : files) {
-			Document document = new Document();
+			Document doc = new Document();
 
 			String path = file.getCanonicalPath();
 
-			indexWriter.addDocument(document);
+			indexWriter.addDocument(doc);
 
-			Document doc = new Document();
-
-			// ===================================================
+		    // ===================================================
 			// add contents of file
 			// ===================================================
 			FileReader fr = new FileReader(file);
+			
 			doc.add(new TextField("contents", fr));
 			doc.add(new StringField("path", file.getPath(), Field.Store.YES));
 			doc.add(new StringField("filename", file.getName(), Field.Store.YES));
@@ -69,17 +77,54 @@ public class LuceneIndexer {
 
 		indexWriter.close();
 	}
+	
+	public static void searchIndex(String searchString) {
+
+		System.out.println("Searching.... '" + searchString + "'");
+
+		try {
+			Analyzer analyzer = new StandardAnalyzer();
+			
+			
+			FSDirectory dir = FSDirectory.open(new File(indexDir).toPath());
+
+			IndexReader indexReader;
+			indexReader=DirectoryReader.open(dir);
+			
+			IndexSearcher searcher = new IndexSearcher(indexReader);
+
+			
+
+			QueryParser qp = new QueryParser("contents",
+					analyzer);
+			Query query = qp.parse(searchString); // parse the query and construct the Query object
+
+			TopDocs hits = searcher.search(query,100); // run the query
+			System.out.println("Found: " + hits.totalHits);
+			
+            int i=0;
+			for(ScoreDoc scoreDoc:hits.scoreDocs)
+			{
+				Document doc=searcher.doc(scoreDoc.doc);
+				System.out.println(++i+" : "+doc.get("path"));
+			}
+		
+			indexReader.close(); 
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+}
 
 	public static void main(String[] args) throws CorruptIndexException, LockObtainFailedException, IOException {
 		// TODO Auto-generated method stub
 		
-		int year=2006;
-		//String indexFolder=StaticInfo.BUGDIR+"/"+year+"-index";
-		//String docFolder=StaticInfo.BUGDIR+"/"+year+"-Preprocessed";
-		//new LuceneIndexer(docFolder, indexFolder).createIndex();
+		
 		String indexFolder="./Data/Index";
 		String docFolder="/Users/user/Documents/Backup/ProcessedFiles/";
-		new LuceneIndexer(docFolder, indexFolder).createIndex();
+		//new LuceneIndexer(docFolder, indexFolder).createIndex();
+		new LuceneIndexer(docFolder, indexFolder).searchIndex("bind");
 		
 	}
 
--- src/lucene/TFIDFCalculator.java
+++ src/lucene/TFIDFCalculator.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.MultiFields;
 import org.apache.lucene.index.Term;
@@ -19,6 +20,8 @@ import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.packed.PackedInts.Writer;
 
+import utility.MiscUtility;
+
 
 
 public class TFIDFCalculator {
@@ -27,12 +30,16 @@ public class TFIDFCalculator {
 	HashMap<String, Double> tfMap;
 	HashMap<String, Double> dfMap;
 	HashMap<String, Double> tfidfMap;
-
+	private long totalTermFreqCorpus;
+	private ArrayList<String> keys;
+	public static String FIELD_CONTENTS = "contents";
+	
 	public TFIDFCalculator(String indexDir) {
 		this.indexDir = indexDir;
 		this.tfidfMap = new HashMap<String, Double>();
 		this.tfMap = new HashMap<String, Double>();
 		this.dfMap = new HashMap<String, Double>();
+		
 	}
 
 	public TFIDFCalculator() {
@@ -41,6 +48,8 @@ public class TFIDFCalculator {
 		this.tfidfMap = new HashMap<String, Double>();
 		this.tfMap = new HashMap<String, Double>();
 		this.dfMap = new HashMap<String, Double>();
+		this.FIELD_CONTENTS="contents";
+		this.keys=new ArrayList<>();
 	}
 
 	public HashMap<String, Double> getDF(ArrayList<String> tokens)
@@ -75,12 +84,55 @@ public class TFIDFCalculator {
 		}
 		return dfMap;
 	}
-
 	
 
+	public HashMap<String, Long> calculateTF() {
+			HashMap<String, Long> termFreqMap = new HashMap<>();
+			try {
+				IndexReader reader = DirectoryReader.open(FSDirectory
+						.open(new File(indexDir).toPath()));
+				// String targetTerm = "breakpoint";
+
+				Fields fields = MultiFields.getFields(reader);
+				for (String field : fields) {
+					Terms terms = fields.terms(field);
+					TermsEnum termsEnum = terms.iterator();
+					BytesRef bytesRef;
+					while ((bytesRef = termsEnum.next()) != null) {
+						if (termsEnum.seekExact(bytesRef)) {
+							String term = bytesRef.utf8ToString();
+							this.keys.add(term);
+						}
+					}
+				}
+
+				for (String term : this.keys) {
+					Term t = new Term(FIELD_CONTENTS, term);
+					// calculating the TF
+					long totalTermFreq = reader.totalTermFreq(t);
+					if (!termFreqMap.containsKey(term)) {
+						termFreqMap.put(term, totalTermFreq);
+						totalTermFreqCorpus += totalTermFreq;
+					}
+				}
+			} catch (Exception exc) {
+				// handle the exception
+			}
+			
+			MiscUtility.showResult(10, termFreqMap);
+			return termFreqMap;
+	}
 	public static void main(String[] args) {
 		
-		
+		TFIDFCalculator obj=new TFIDFCalculator();
+		obj.calculateTF();
+		try {
+			obj.getDF(obj.keys);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//new TFIDFCalculator().calculateTF();
 	}
 
 }
