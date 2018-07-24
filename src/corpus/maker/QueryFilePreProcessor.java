0496337ca0b81b65dc46de0579702a5fbfba0233
Shamima Yeasmin <muktacseku@gmail.com>
2018-07-24 16:56:51 -0600

    Simiscore Calculation is done

---
+++ src/corpus/maker/QueryFilePreProcessor.java
@@ -0,0 +1,37 @@
+package corpus.maker;
+
+import java.io.File;
+
+import utility.ContentLoader;
+import utility.ContentWriter;
+
+public class QueryFilePreProcessor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new QueryFilePreProcessor().nameChangedandContent("/Users/user/Documents/Backup/QueryData/", "/Users/user/Documents/Ph.D/2018/Data/BugData/");
+	}
+
+	public void nameChangedandContent(String queryFilePath, String outFolder)
+	{
+		File[] bugFiles = new File(queryFilePath).listFiles();
+		System.out.println("Total number of query files: "+bugFiles.length);
+		int noOfTotalDocument=bugFiles.length;
+		for (File bugFile : bugFiles) {
+			String queryContent = ContentLoader.readContentSimple(bugFile
+				.getAbsolutePath());
+			//System.out.println(sourceCodeContent);
+			String[] spilter=queryContent.split(" ");
+			if(spilter.length>=2)
+			{
+				String content="";
+				for(int i=1;i<spilter.length;i++)
+				{
+				 	content+=spilter[i]+" ";
+				}
+				ContentWriter.writeContent(outFolder+bugFile.getName(),content);
+			}
+		}
+	}
+	
+}
--- src/simi/score/calculator/SimiScoreCalc.java
+++ src/simi/score/calculator/SimiScoreCalc.java
@@ -1,6 +1,7 @@
 package simi.score.calculator;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import lucene.VSMCalculator;
@@ -11,60 +12,125 @@ public class SimiScoreCalc {
 
 	public String queryContent;
 	public String sourceCodeFile;
+	public String bugFile;
 	public HashMap<String, String> sourceContentHM;
-	
-	public SimiScoreCalc(String sourceCodeFile, String queryContent)
+	public HashMap<String, String> bugContentHM;
+	public HashMap<String, Double> cosineScoreHM;
+	public HashMap<String, Double> similarityScoreHM;
+	HashMap<Integer, ArrayList<String>> goldMap;
+ 	
+	public SimiScoreCalc(String sourceCodeFile, String queryContent, String bugFile)
 	{
 		this.sourceCodeFile=sourceCodeFile;
 		this.queryContent=queryContent;
+		this.bugFile=bugFile;
 		this.sourceContentHM=new HashMap<>();
+		this.bugContentHM=new HashMap<>();
+		this.cosineScoreHM=new HashMap<>();
+		this.similarityScoreHM=new HashMap<>();
 	}
 	
-	public void LoadSourceCodeFiles()
+	protected HashMap<Integer, ArrayList<String>> loadGoldsetMap(String goldFile) {
+		HashMap<Integer, ArrayList<String>> goldMap = new HashMap<>();
+		ArrayList<String> lines = ContentLoader.getAllLinesOptList(goldFile);
+		for (int i = 0; i < lines.size();) {
+			String[] parts = lines.get(i).split("\\s+");
+			if (parts.length == 2) {
+				int bugID = Integer.parseInt(parts[0].trim());
+				int bugCount = Integer.parseInt(parts[1].trim());
+				for (int j = i + 1; j <= i + bugCount; j++) {
+					if (goldMap.containsKey(bugID)) {
+						ArrayList<String> temp = goldMap.get(bugID);
+						temp.add(lines.get(j).trim());
+						goldMap.put(bugID, temp);
+					} else {
+						ArrayList<String> temp = new ArrayList<>();
+						temp.add(lines.get(j).trim());
+						goldMap.put(bugID, temp);
+					}
+				}
+				i = i + bugCount + 1;
+			}
+		}
+		return goldMap;
+	}
+	
+	
+	public HashMap<String, String> LoadFiles(String filePath)
 	{
-		File[] sourceCodeFiles = new File(this.sourceCodeFile).listFiles();
+		HashMap<String, String> hm=new HashMap<>();
+		File[] sourceCodeFiles = new File(filePath).listFiles();
 		System.out.println("Total number of source code files: "+sourceCodeFiles.length);
 		int noOfTotalDocument=sourceCodeFiles.length;
 		
 		for (File sourceCodeFile : sourceCodeFiles) {
 			String sourceCodeContent = ContentLoader.readContentSimple(sourceCodeFile
 				.getAbsolutePath());
-			this.sourceContentHM.put(sourceCodeFile.getName(), sourceCodeContent);
+			hm.put(sourceCodeFile.getName(), sourceCodeContent);
 			
 		}
-		MiscUtility.showResult(10, this.sourceContentHM);
+		//MiscUtility.showResult(10, hm);
+		return hm;
 	}
 	 
 	public void cosineSimiCalculator() {
-			for (String key : this.sourceContentHM.keySet()) {
+			for (String bugID : this.bugContentHM.keySet()) {
 			
 				String str1=this.queryContent;
-				String str2=this.sourceContentHM.get(key);
-				double cosineSimilarity = new CosineSimilarity().getSimilarity(
+				String str2=this.bugContentHM.get(bugID);
+				double cosineSimilarity = new CosineSimilarity().similarity(
 						str1,str2);
 						//queryContent, bugContent);
 				//System.out.println(cosineSimilarity);
 				if (cosineSimilarity > 0) {
-					String bugFileID = bugFile.getName();
-					String bugID = bugFileID.substring(0,
-							bugFileID.length() - 4);
-					//System.out.println(bugID+" "+cosineSimilarity);
-					this.hm.put(bugID, cosineSimilarity);
-					//System.out.println(bugID+" "+cosineSimilarity);
+					String bugIDfound = bugID;
+					
+					//System.out.println(bugIDfound+" "+cosineSimilarity);
+					this.cosineScoreHM.put(bugIDfound, cosineSimilarity);
 				}
-				//System.out.println(bugFile+" is done");
 			}
-			String content=simiScoreCalculator();
-			String queryID=queryFile.getName();
-			
-			
+	}
+	
+	
+	public void SimilarityCalc()
+	{
+		for(String bugID:this.cosineScoreHM.keySet())
+		{ 
+			//System.out.println(bugID);
+			if(bugID.equalsIgnoreCase(".DS_Store")==false){
+			int ID=Integer.valueOf(bugID.substring(0, bugID.length()-4));
+			if(this.goldMap.containsKey(ID)){
+				//System.out.println(this.goldMap.get(ID));
+				int n_i=this.goldMap.get(ID).size();
+				for(String file:this.goldMap.get(ID)){
+					if(this.similarityScoreHM.containsKey(file))
+					{
+						double simiScoreOld=this.similarityScoreHM.get(file);
+						double simiScoreSi=simiScoreOld+this.cosineScoreHM.get(bugID)/Double.valueOf(n_i);
+						this.similarityScoreHM.put(file, simiScoreSi);
+					
+					}
+					else
+					{
+						double simiScoreSi=this.cosineScoreHM.get(bugID)/Double.valueOf(n_i);
+						this.similarityScoreHM.put(file, simiScoreSi);
+					}
+				}
+			}
 		}
 	}
+		MiscUtility.showResult(20, MiscUtility.sortByValues(this.similarityScoreHM));
+}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		SimiScoreCalc obj=new SimiScoreCalc("/Users/user/Documents/Ph.D/2018/Data/SourceForBL/", "/Users/user/Documents/Ph.D/2018/Data/BugData/35427.txt", "/Users/user/Documents/Ph.D/2018/Data/BugData/");
+		obj.sourceContentHM=obj.LoadFiles(obj.sourceCodeFile);
+		obj.bugContentHM=obj.LoadFiles(obj.bugFile);
+		obj.goldMap=obj.loadGoldsetMap("./Data/gitInfoNew.txt");
+		obj.cosineSimiCalculator();
+		obj.SimilarityCalc();
 	}
 
 	
